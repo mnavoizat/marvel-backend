aeb33d2b086b4c2c3087b6d78a12dafab0b79acc
Maxime Navoizat <maximenavoizat@gmail.com>
2020-11-18 15:45:57 +0100

    test

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ app.use(cors());
 const public_API_key = process.env.PUBLIC_API_KEY;
 const private_API_key = process.env.PRIVATE_API_KEY;
 
-app.get("/test", async (req, res) => {
+app.get("/test", (req, res) => {
+  try {
+    res.json("bien reçu");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get("/comics", async (req, res) => {
   try {
     const date = new Date();
     const timestamp = date.getTime();
@@ -20,6 +28,22 @@ app.get("/test", async (req, res) => {
 
     const response = await axios.get(
       `https://gateway.marvel.com/v1/public/comics?ts=${timestamp}&apikey=${public_API_key}&hash=${hash}`
+    );
+
+    res.json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get("/characters", async (req, res) => {
+  try {
+    const date = new Date();
+    const timestamp = date.getTime();
+    const hash = md5(timestamp + private_API_key + public_API_key);
+
+    const response = await axios.get(
+      `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${public_API_key}&hash=${hash}`
     );
 
     res.json(response);
